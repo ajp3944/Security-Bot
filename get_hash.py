8893d74e71c34878afc821ee6b36e734c9d5034e
ReconRC <brayanmena6@gmail.com>
2022-09-10 18:48:21 -0400

    Added decode/encode for base64/32 and urlshortener

--- get_hash.py
+++ get_hash.py
@@ -5,6 +5,7 @@ import hashlib
 file_url = 'https://cdn.discordapp.com/attachments/1018003056077574195/1018245724552564888/sillyfile.txt'
 api_key = '93d85cbb593276a2a1eec152078bcd97df0c84ff42bd092b14455aa9ff152b3f'
 
+## Searches the type of hash
 def get_hash(user_input):
     ip_address = user_input
     response = requests.get(f'https://freerestapi.herokuapp.com/api/hash-identifier?hash={user_input}').json()
@@ -18,7 +19,7 @@ def get_hash(user_input):
     }
     return hash_data
 
-
+## Used for uploading file to virustotal
 def upload_file(file):
     data = requests.get(file)
     hash = hashlib.sha256(data.text.encode('UTF-8')).hexdigest()
@@ -29,7 +30,7 @@ def upload_file(file):
 
     return hash
 
-
+## Used for retrieving information about a file from virustotal
 def file_info():
     id = upload_file(file_url)
     url = f'https://www.virustotal.com/api/v3/files/{str(id)}'
