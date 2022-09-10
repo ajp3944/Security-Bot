7e644e747fa7451f35d79929d2d801307b5416d9
victorz <victormzapataiii@gmail.com>
2022-09-10 11:21:49 -0400

    bot.py update

--- bot.py
+++ bot.py
@@ -1,4 +1,32 @@
 import hikari
+import lightbulb
 
-bot = hikari.GatewayBot(token='MTAxNzk5Njk2MjI2NTU3NTQ5NQ.GS-8at.Xw6bpIjLad4hcvCURXDbrVJ-ic3FavcY_fuyGU')
+bot = lightbulb.BotApp(
+    token='MTAxNzk5Njk2MjI2NTU3NTQ5NQ.GxrtRN.o-LJ-grUAAK-GikSyuHjd0vjkgwsN9C3OEvby8', default_enabled_guilds=(1017526173061881946))
+
+#.\env\Scripts\activate
+#python bot.py
+
+@bot.listen(hikari.GuildMessageCreateEvent)
+async def print_message(event):
+    print(event.author.username + " " + event.content)
+
+@bot.listen(hikari.StartedEvent)
+async def bot_started(event):
+    print('Bot has started!')
+
+@bot.command
+@lightbulb.command('ping', 'Say pong!')
+@lightbulb.implements(lightbulb.SlashCommand)
+async def ping(ctx):
+    await ctx.respond('Pong!')
+
+@bot.command
+@lightbulb.option('num1', 'The first number', type = int)
+@lightbulb.option('num2', 'The second number', type = int)
+@lightbulb.command('add', 'Add two numbers together')
+@lightbulb.implements(lightbulb.SlashCommand)
+async def add(ctx):
+    await ctx.respond(ctx.options.num1 + ctx.options.num2)
+
 bot.run()
