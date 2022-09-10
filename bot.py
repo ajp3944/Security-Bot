9f9a37337a2cbc3fd60ac9f762d7c3e4a3058db5
VictorZapataIII <68867269+VictorZapataIII@users.noreply.github.com>
2022-09-10 11:25:04 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ import hikari
 import lightbulb
 
 bot = lightbulb.BotApp(
-    token='MTAxNzk5Njk2MjI2NTU3NTQ5NQ.GxrtRN.o-LJ-grUAAK-GikSyuHjd0vjkgwsN9C3OEvby8', default_enabled_guilds=(1017526173061881946))
+    token='', default_enabled_guilds=(1017526173061881946))
 
 #.\env\Scripts\activate
 #python bot.py
