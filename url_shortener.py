ecd9eac386f0daf55d7f902278f5dd27569a4046
ReconRC <brayanmena6@gmail.com>
2022-09-10 18:49:32 -0400

    Added decode/encode for base64/32 and urlshortener

---
+++ url_shortener.py
@@ -0,0 +1,17 @@
+import requests
+
+def file_info(user_input):
+    url = "https://url-shortener-service.p.rapidapi.com/shorten"
+    payload = f'url={user_input}'
+    print(user_input)
+    headers = {
+	"content-type": "application/x-www-form-urlencoded",
+	"X-RapidAPI-Key": "2efad7f699msh835311fb7809de6p1bc0dajsn915f78e41a14",
+	"X-RapidAPI-Host": "url-shortener-service.p.rapidapi.com"
+}
+
+    response = requests.request("POST", url, data=payload, headers=headers)
+
+    print(response.text)
+
+file_info('https://google.com')
