1e87d7bb724aaa9d64f7ff2b378609f664633016
VictorZapataIII <68867269+VictorZapataIII@users.noreply.github.com>
2022-09-10 20:15:08 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,7 +8,7 @@ import phone_number_validator
 import get_hash
 
 bot = lightbulb.BotApp(
-    token='MTAxNzk5Njk2MjI2NTU3NTQ5NQ.GxrtRN.o-LJ-grUAAK-GikSyuHjd0vjkgwsN9C3OEvby8', default_enabled_guilds=(1017526173061881946))
+    token='', default_enabled_guilds=(1017526173061881946))
 
 #.\env\Scripts\activate
 #python bot.py
